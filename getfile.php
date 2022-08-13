1699f4a1d07143f51ff7d4ffd34085d661aef2e2
Raghu02496 <108466616+Raghu02496@users.noreply.github.com>
2022-08-13 22:30:48 +0530

    Update getfile.php

--- getfile.php
+++ getfile.php
@@ -2,6 +2,6 @@
 require("validsession.php");
   header('Content-Type: image/png');
   header("Content-type: application/pdf");
-  readfile("\\\\DESKTOP-TPUIMSV\\uploaded_files\\".$_SESSION['email'].'\\'. $_GET['img']);
+  readfile("\\\\DESKTOP\\uploaded_files\\".$_SESSION['email'].'\\'. $_GET['img']);
   // readfile("C:\Users\R\Pictures\Wallpapers\\". $_GET['img']);
 ?>
