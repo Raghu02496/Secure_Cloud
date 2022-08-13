40fce75d31ba5629b4140828304c3867afd0c344
Raghu02496 <108466616+Raghu02496@users.noreply.github.com>
2022-08-13 22:31:44 +0530

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -4,7 +4,7 @@ require("validsession.php");
  $to=$_SESSION['email'];
  $subject="Email Verification";
  $message="Your OTP Is ".$_SESSION['otp']."\n\nSimply Ignore This Mail If You Havenen't Signed Up For Our Page.";
- $from="From: projectyourpetcare@gmail.com \r\n";
+ $from="From: @gmail.com \r\n";
  $headers="Content-Type: text/html; charset=UTF-8" . "\r\n";
  $headers="MIME-Version: 1.0" . "\r\n";
         
