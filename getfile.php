289d3bc0f88331b5652298253d6188135056d0fc
Raghu02496 <108466616+Raghu02496@users.noreply.github.com>
2022-08-23 16:48:35 +0530

    Delete getfile.php

--- getfile.php
+++
@@ -1,7 +0,0 @@
-<?php
-require("validsession.php");
-  header('Content-Type: image/png');
-  header("Content-type: application/pdf");
-  readfile("\\\\DESKTOP\\uploaded_files\\".$_SESSION['email'].'\\'. $_GET['img']);
-  // readfile("C:\Users\R\Pictures\Wallpapers\\". $_GET['img']);
-?>
